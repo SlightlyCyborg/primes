00faf89e562d352f3f1f9d79f174d09f8f280e22
slightlycyborg <collinalexbell@gmail.com>
2018-12-07 20:14:00 +0000

    init commit

---
+++ src/funding_circle/primes.clj
@@ -0,0 +1,43 @@
+(ns funding-circle.primes)
+
+(defn new-prime-cache []
+  (atom (sorted-set 2)))
+
+(defn primes-<= [last-prime primes]
+  (subseq primes <= last-prime))
+
+(defn divides-evenly?
+  "returns true only if dividend is divided evenly by divisor"
+  [dividend divisor]
+  (= 0 (mod dividend divisor)))
+
+(defn divides-evenly-by-any?
+  "returns true if dividend is divided evenly by any of the divisors"
+  [dividend divisors]
+  (reduce #(or %1 (dividesEvenly? dividend %2)) 
+          false
+          divisors))
+
+(defn get-next-prime
+  "returns next prime based on last prime and prime cache"
+  [last-prime prime-cache-atom]
+  (when (not (contains? @prime-cache-atom last-prime))
+    (throw (Exception. "Last prime hasn't been generated yet")))
+
+  (loop [next-prime (+ 1 last-prime)]
+    (if (divides-evenly-by-any? next-prime (primes-<= last-prime
+                                                      @prime-cache-atom))
+        (recur (+ 1 next-prime))
+        (do
+          (swap! prime-cache-atom conj next-prime)
+          next-prime))))
+
+(defn gen-primes 
+  "returns a lazy seq of primes"
+  ([]
+   (let [prime-cache (new-prime-cache)]
+     (cons 2 (gen-primes 2 prime-cache))))
+  ([last-prime prime-cache]
+   (let [this-prime (get-next-prime last-prime prime-cache)]
+     (lazy-seq (cons this-prime (gen-primes this-prime prime-cache))))))
+
---
+++ test/funding_circle/primes_test.clj
@@ -0,0 +1,26 @@
+(ns funding-circle.primes-test
+  (:use [clojure.test :as test]
+        [funding-circle.primes :as primes]))
+
+
+(deftest test-divides-evenly?
+  (is (primes/divides-evenly? 6 3))
+  (is (not (primes/divides-evenly? 6 5))))
+
+
+(deftest test-divides-evenly-by-any?
+  (is (divides-evenly-by-any? 3 [1 2 3]))
+  (is (not (divides-evenly-by-any? 3 [6 4 5]))))
+
+
+(deftest test-get-next-prime
+  (let [prime-cache (atom (sorted-set 2))]
+   (is (= 3 (primes/get-next-prime 2 prime-cache)))
+   (is (= 5 (primes/get-next-prime 3 prime-cache)))
+   (is (= 7 (primes/get-next-prime 5 prime-cache)))
+   ;;If all previous primes hav not been gen'd yet, an exception will be thrown
+   (is (thrown? Exception (primes/get-next-prime 19 prime-cache)))))
+
+
+(deftest test-gen-next-prime
+  (is (= (take 10 (gen-primes)) '(2 3 5 7 11 13 17 19 23 29))))
